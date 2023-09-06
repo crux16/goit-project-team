bfadb8704791909a5e4adfefa23da8303009238d
GetTheApp <riayap1@gmail.com>
2023-09-06 21:46:39 +0800

    Header
    ok

---
+++ src/index.js
@@ -0,0 +1,27 @@
+(() => {
+  const mobileMenu = document.querySelector('.menu-box');
+  const openMenuBtn = document.querySelector('.open-menu');
+  const closeMenuBtn = document.querySelector('.pages');
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+      openMenuBtn.classList.toggle('is-toggle');
+      mobileMenu.classList.toggle('is-open');
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+  openMenuBtn.addEventListener('click', toggleMenu);
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    openMenuBtn.classList.remove('is-toggle');
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
